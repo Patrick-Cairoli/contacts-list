8e6ea067942c3204cdc08dc35ab182b296221eab
unknown <patrick.cairoli@gmail.com>
2022-03-11 19:31:44 -0800

    corrected bug that prevented fetching new users after displaying the table with user details

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-<div className="d-flex justify-content-center my-4">
-    <div className="spinner-border text-info" role="status">
+    <div className="d-flex justify-content-center my-4">
+      <div className="spinner-border text-info" role="status">
         <span className="visually-hidden">Loading...</span>
+      </div>
     </div>
-</div>
   )
 }
 
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,37 +1,42 @@
-import React from 'react'
+import React from 'react';
 
-const Table = ( {tableData, selectedUser} ) => {
+const Table = ({ tableData, selectedUser }) => {
+  
+	const filtered = tableData?.find((el) => el.id === selectedUser);
 
-  const filtered = tableData?.find(el => el.id === selectedUser)
+	// const { userName, password, name, gender, email, phone } = filtered;
 
-  const {userName, password, name, gender, email, phone } = filtered;
+	return (
+		<div className="table-container">
+			<table className="table table-dark table-striped">
+				<thead>
+					<tr>
+						<th scope="col">Name</th>
+						<th scope="col">Gender</th>
+						<th scope="col">Email</th>
+						<th scope="col">Phone</th>
+						<th scope="col">User Name</th>
+						<th scope="col">Password</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">{filtered?.name}</th>
+						<td>
+							{filtered?.gender === 'male' ? (
+								<i className="fa-solid fa-mars text-info"></i>
+							) : (<i className="fa-solid fa-venus text-danger"></i>
+							)}
+						</td>
+						<td>{filtered?.email}</td>
+						<td>{filtered?.phone}</td>
+						<td>{filtered?.userName}</td>
+						<td>{filtered?.password}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+};
 
-  return (
-    <div className="table-container">
-      <table className="table table-dark table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Name</th>
-      <th scope="col">Gender</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone</th>
-      <th scope="col">User Name</th>
-      <th scope="col">Password</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">{name}</th>
-      <td>{gender === "male" ? (<i className="fa-solid fa-mars text-info"></i>) : (<i className="fa-solid fa-venus text-danger"></i>)}</td>
-      <td>{email}</td>
-      <td>{phone}</td>
-      <td>{userName}</td>
-      <td>{password}</td>
-    </tr>
-  </tbody>
-</table>
-    </div>
-  )
-}
-
-export default Table
+export default Table;
